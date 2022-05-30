3b703e122fe4697b6c8e8672e444ffa99bb40bb5
renpereiradx <renedx92@gmail.com>
2022-05-30 10:08:12 -0400

    Menu Pedido concluido y leccion Estructura Switch

--- estructuraSwitch.c
+++ estructuraSwitch.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
-int optOne = 1;
-int optTwo = 0;
+int optOne = 0;
+int optTwo = 1;
 
 int main()
 {
@@ -9,55 +8,69 @@ int main()
     printf("opcion 0 para platzi bebidas \n");
     printf("opcion 1 para platzi comidas \n");
     printf("opcion 2 para platzi postres \n");
+  
 
-    // este programa genera menus segun lo que el usuario elija
-    if (optOne == 0)
+    switch (optOne)
     {
+    case 0:
         printf("usted eligio la opcion 0, vera nuestro menu de bebidas elija una:\n");
         printf("opcion 0 para platzi cola cero \n");
         printf("opcion 1 para platzi cola normal \n");
         printf("opcion 2 para platzi pina colada \n");
-
-        if (optTwo == 0)
+        switch (optTwo)
+        {
+        case 0:
             printf("usted eligio una platzi cola cero, mmm rico");
-        else if (optTwo == 1)
+            break;
+        case 1:
             printf("usted eligio una platzi cola cpn azucar, mmm rico");
-        else if (optTwo == 2)
+            break;
+        case 2:
             printf("usted eligio una platzi pina colada con azucar, mmm rico");
-        else
+            break;
+        default:
             printf("opcion invalida");
-    }
-    else if (optOne == 1)
-    {
+            break;
+        }
+        break;
+    case 1:
         printf("usted ha elegido la opcion 1, vera nuestro menu de comidas, elija una: \n");
         printf("opcion 0 para platzi burger \n");
         printf("opcion1 para platzi pizza \n");
-
-        if (optTwo == 0)
+        switch (optTwo)
+        {
+        case 0:
             printf("Disfrute de su platzi burger");
-        else if (optTwo == 1)
+            break;
+        case 1:
             printf("Disfrute de su platzi pizza");
-        else
+            break;
+        default:
             printf("No ha hecho ninguna eleccion. Lamentamos que no haya lo que a usted le guste");
-    }
-    else if (optOne == 2)
-    {
+            break;
+        }
+        break;
+    case 2:
         printf("usted ha elegido la opcion 2, vera nuestro menu de postres, elija una: \n");
         printf("opcion 0 para platzi macaron \n");
         printf("opcion1 para platzi helado \n");
-
-        if (optTwo == 0)
+        switch (optTwo)
+        {
+        case 0:
             printf("Disfrute de su platzi flan");
-        else if (optTwo == 1)
+            break;
+        case 1:
             printf("Disfrute de su platzi helado");
-        else
+            break;
+        default:
             printf("No ha seleccionado nada :(");
-    }
-    else
-    {
+            break;
+        }
+        break;
+    default:
         printf("No ha hecho una eleccion :( . Vuelva pronto :)");
+        break;
     }
-
     printf("\n====================================================\n");
     return 0;
 }
